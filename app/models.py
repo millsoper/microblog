9d707ff715d44733739be6c8e6d00db0383a0f93
jmsoper <jmsoper@gmail.com>
2018-07-06 16:39:32 -0400

    wiring up login, auth, redirects

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
+from flask_login import UserMixin
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(64), index=True, unique=True)
   email = db.Column(db.String(12), index=True, unique=True)
@@ -17,6 +18,10 @@ class User(db.Model):
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
 
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key=True)
