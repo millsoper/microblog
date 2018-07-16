25289857fb16665b65e4afa0a2166c23b3d7906b
jmsoper <jmsoper@gmail.com>
2018-07-16 17:04:32 -0400

    debug password reset email

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from time import time
+import jwt
 from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import db, login
+from app import db, login, app
 
 class User(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -22,7 +24,7 @@ class User(UserMixin, db.Model):
   def get_reset_password_token(self, expires_in=600):
     return jwt.encode(
       {'reset_password': self.id, 'exp': time() + expires_in},
-      app.config['SECRET_KEY'], algorithm='HS256'.decode('utf-8')
+      app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
   @staticmethod
   def verify_reset_password_token(token):
