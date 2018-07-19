f4f8ee8ae19d3e1b762795ce0952cfeb708f70d3
jmsoper <jmsoper@gmail.com>
2018-07-18 20:31:46 -0400

    massive restructuring, pre-testing

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,20 +1,19 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user, logout_user, login_required
-from werkzeug.urls import url_parse
 from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request, current_app
+from flask_login import current_user, login_required
 from app.models import User, Post
-from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, EditPostForm
-from app.email import send_password_reset_email
+from app import db
+from app.main.forms import EditProfileForm, PostForm, EditPostForm
+from app.main import bp
 
-@app.before_request
+@bp.before_app_request
 def before_request():
   if current_user.is_authenticated:
     current_user.last_seen = datetime.utcnow()
     db.session.commit()
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@bp.route('/', methods=['GET', 'POST'])
+@bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
@@ -34,50 +33,14 @@ def index():
     return render_template('index.html', title='Home', posts=posts.items,
                             form=form, next_url=next_url, prev_url=prev_url) 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  if current_user.is_authenticated:
-    return redirect(url_for('index'))
-  form = LoginForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(username=form.username.data).first()
-    if user is None or not user.check_password(form.password.data):
-      flash('Invalid username or password')
-      return redirect(url_for('login'))
-    login_user(user, remember=form.remember_me.data)
-    next_page = request.args.get('next')
-    if not next_page or url_parse(next_page).netloc != '':
-      next_page = url_for('index')
-    return redirect(next_page)
-  return render_template('login.html', title='Sign In', form=form)
-
-@app.route('/logout')
-def logout():
-  logout_user()
-  return redirect(url_for('index'))
-
-@app.route('/delete_post')
+@bp.route('/delete_post')
 def delete_post():
   postId = request.args.get('postId')
   Post.query.filter(Post.id == postId).delete()
   db.session.commit()
   return redirect(url_for('index'))
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-  if current_user.is_authenticated:
-    return redirect(url_for('index'))
-  form = RegistrationForm()
-  if form.validate_on_submit():
-    user = User(username=form.username.data, email=form.email.data)
-    user.set_password(form.password.data)
-    db.session.add(user)
-    db.session.commit()
-    flash('Congratulations, you are now a registered user!')
-    return redirect(url_for('login'))
-  return render_template('register.html', title='Register', form=form)
-
-@app.route('/user/<username>')
+@bp.route('/user/<username>')
 @login_required
 def user(username):
   user = User.query.filter_by(username=username).first_or_404()
@@ -91,11 +54,11 @@ def user(username):
   return render_template('user.html', user=user, posts=posts.items, 
                         next_url=next_url, prev_url=prev_url)
 
-@app.route('/about')
+@bp.route('/about')
 def about():
   return render_template('about.html', title='About')
 
-@app.route('/delete_profile')
+@bp.route('/delete_profile')
 def delete_profile():
   id = request.args.get('id')
   logout_user()
@@ -106,7 +69,7 @@ def delete_profile():
   db.session.commit()
   return redirect(url_for('index'))
 
-@app.route('/edit_profile', methods=['GET', 'POST'])
+@bp.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
   form = EditProfileForm(current_user.username)
@@ -121,7 +84,7 @@ def edit_profile():
   return render_template('edit_profile.html', title='Edit Profile',
                          form=form)
 
-@app.route('/edit_post', methods=['GET', 'POST'])
+@bp.route('/edit_post', methods=['GET', 'POST'])
 @login_required
 def edit_post():
   postId = request.args.get('postId')
@@ -142,31 +105,3 @@ def edit_post():
     form.title.data = givenPost.title
     form.month.data = oldDate.month
   return render_template('edit_post.html', title='Edit Post', form=form)
-
-@app.route('/reset_password_request', methods=['GET', 'POST'])
-def reset_password_request():
-  if current_user.is_authenticated:
-    return redirect(url_for('index'))
-  form = ResetPasswordRequestForm()
-  if form.validate_on_submit():
-    user = User.query.filter_by(email=form.email.data).first()
-    if user:
-      send_password_reset_email(user)
-    flash('Check your email for the instructions to reset your password')
-    return redirect(url_for('login'))
-  return render_template('reset_password_request.html', title='ResetPassword', form=form)
-
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-  if current_user.is_authenticated:
-    return redirect(url_for('index'))
-  user = User.verify_reset_password_token(token)
-  if not user:
-    return redirect(url_for('index'))
-  form = ResetPasswordForm()
-  if form.validate_on_submit():
-    user.set_password(form.password.data)
-    db.session.commit()
-    flash('Your password has been reset.')
-    return redirect(url_for('login'))
-  return render_template('reset_password.html', form=form)
